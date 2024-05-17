a9f18f71e1dc86916e268d529f6a9a8322d3dc32
Ambroise THIBAULT <92878091+AmbroiseTHIBAULT@users.noreply.github.com>
2024-05-17 10:54:10 +0200

    add random state

--- options_pricing/scripts/data_processing.py
+++ options_pricing/scripts/data_processing.py
@@ -68,7 +68,7 @@ def create_train_test_set(X, y, test_size, standardize, df):
         X_test = df.drop(X.index)[X.columns].to_numpy()
         y_test = df.drop(y.index)[y.name].to_numpy()
     else:
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
     # Standardize data
     scaler = None
     if standardize:
